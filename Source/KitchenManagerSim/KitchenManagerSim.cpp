1a3f0466546c54cbad97abb821eba5b38ac7d43b
Janner Conde <condejanner@gmail.com>
2021-02-06 23:59:38 -0700

    Add unreal project start

---
+++ Source/KitchenManagerSim/KitchenManagerSim.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KitchenManagerSim.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KitchenManagerSim, "KitchenManagerSim" );
---
+++ Source/KitchenManagerSim/KitchenManagerSimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KitchenManagerSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KITCHENMANAGERSIM_API AKitchenManagerSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
